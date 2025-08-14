3fd7b31aa7408ddc3d0e2c88cbe1889ec5047805
DXklvov <k.lvov@deaxo.com>
2025-08-14 21:39:47 +0300

    init + SurvivalMode 1.20.12

---
+++ copy.ps1
@@ -0,0 +1,3 @@
+Copy-Item -Path "D:\UnPixeled\UnPixeledSurvival\obj\Debug\net7.0\ref\VSSurvivalMod.dll" -Destination "D:\UnPixeled\UnPixeledGame\Mods\VSSurvivalMod.dll" -Recurse
+Copy-Item -Path "D:\UnPixeled\UnPixeledEssentialsMod\obj\Debug\net7.0\ref\VSEssentials.dll" -Destination "D:\UnPixeled\UnPixeledGame\Mods\VSEssentials.dll" -Recurse
+Copy-Item -Path "D:\UnPixeled\UnPixeledCreativeMod\obj\Debug\net7.0\ref\VSCreativeMod.dll" -Destination "D:\UnPixeled\UnPixeledGame\Mods\VSCreativeMod.dll" -Recurse
